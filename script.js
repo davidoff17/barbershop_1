ba4ec85821860bc96050e95b208994c240a8dbb9
davidoff17 <71339123+davidoff17@users.noreply.github.com>
2021-01-08 23:24:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+var link = document.querySelector(".login-link");
+var popup = document.querySelector(".modal-login");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!login.value || !password.value) {
+  evt.preventDefault();
+  popup.classList.add("modal-error");
+} else {
+  localStorage.setItem("login",login.value);
+}
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+    }
+  }
+});
+
+// modal-map
+var mapLink = document.querySelector(".map-button");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+});
